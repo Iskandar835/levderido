dfd5ab5b4310849ac7f22202440c5d0c538f2b97
Sacha Vandermoeten <sachadu835@gmail.com>
2025-03-20 15:25:34 +0100

    chore: optimisation + ajout nouveau lien vers accuei

--- levderido-app/src/components/navbar.tsx
+++ levderido-app/src/components/navbar.tsx
@@ -8,10 +8,11 @@ import {
 import { Bars2Icon } from '@heroicons/react/24/solid'
 import { motion } from 'framer-motion'
 import { Link } from './link'
-import { Logo } from './logo'
+import { Mark } from './logo'
 import { PlusGrid, PlusGridItem, PlusGridRow } from './plus-grid'
 
 const links = [
+  { href: '/', label: 'Accueil' },
   { href: '/histoire', label: 'Histoire' },
   { href: '/inscription', label: 'Inscription' },
   { href: '/contact', label: 'Contact' },
@@ -74,22 +75,13 @@ function MobileNav() {
   )
 }
 
-export function Navbar({ banner }: { banner?: React.ReactNode }) {
+export function Navbar() {
   return (
     <Disclosure as="header" className="pt-12 sm:pt-16">
       <PlusGrid>
-        <PlusGridRow className="relative flex justify-between">
-          <div className="relative flex gap-6">
-            <PlusGridItem className="py-3">
-              <Link href="/" title="Home">
-                <Logo className="h-9" />
-              </Link>
-            </PlusGridItem>
-            {banner && (
-              <div className="relative hidden items-center py-3 lg:flex">
-                {banner}
-              </div>
-            )}
+        <PlusGridRow className="relative flex justify-between lg:justify-end">
+          <div className="flex items-center lg:hidden">
+            <Mark className="h-10" />
           </div>
           <DesktopNav />
           <MobileNavButton />
