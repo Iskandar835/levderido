a3f5712aa86580bb5deac875aa1c7b769baba9a1
Sacha Vandermoeten <sachadu835@gmail.com>
2025-03-20 15:32:08 +0100

    chore: ajustement taille images

--- levderido-app/src/components/logo-cloud.tsx
+++ levderido-app/src/components/logo-cloud.tsx
@@ -13,27 +13,27 @@ export function LogoCloud({
       <img
         alt="Logo théâtre Marc Baron"
         src="/logo-theatres/logo-marc-baron.png"
-        className="h-10 max-sm:mx-auto sm:h-18 lg:h-28"
+        className="h-10 max-sm:mx-auto sm:h-18 lg:h-26"
       />
       <img
         alt="Logo théâtre Daudet"
         src="/logo-theatres/logo-daudet.png"
-        className="h-10 max-sm:mx-auto sm:h-18 lg:h-28"
+        className="h-10 max-sm:mx-auto sm:h-18 lg:h-26"
       />
       <img
         alt="Logo salle Tisot"
         src="/logo-theatres/logo-tisot.png"
-        className="h-10 max-sm:mx-auto sm:h-18 lg:h-28"
+        className="h-10 max-sm:mx-auto sm:h-18 lg:h-26"
       />
       <img
         alt="Logo théâtre Le Colbert"
         src="/logo-theatres/logo-colbert.png"
-        className="h-10 max-sm:mx-auto sm:h-18 lg:h-28"
+        className="h-10 max-sm:mx-auto sm:h-18 lg:h-26"
       />
       <img
         alt="Logo un monde à nous"
         src="/logo-theatres/logo-un-monde-a-nous.png"
-        className="h-10 max-sm:mx-auto sm:h-18 lg:h-28"
+        className="h-10 max-sm:mx-auto sm:h-18 lg:h-26"
       />
     </div>
   )
