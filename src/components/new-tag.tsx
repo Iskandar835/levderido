328d872d84dca791d7776192be69fddfbe46cb94
Sacha Vandermoeten <sachadu835@gmail.com>
2025-09-03 17:01:12 +0200

    feat: ajout composant NewTag avec son style

---
+++ src/components/new-tag.tsx
@@ -0,0 +1,9 @@
+export default function NewTag() {
+  return (
+    <div className="inline-block w-[80px] rounded-md border-2 border-[#059669] bg-[#10B981] px-1 py-1.5 text-center text-white shadow-md lg:w-[90px] lg:p-2">
+      <p className="text-sm font-medium text-white lg:text-base lg:font-semibold">
+        Nouveau
+      </p>
+    </div>
+  )
+}
